9a5edb8bca7ee5ee37b1907fa2b86d1df495b491
Nobuyuki Kimura <a09@r-force.co.jp>
2024-07-23 15:35:13 +0900

    Initial Commit

---
+++ StoveChekcer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoveChekcer
+//
+//  Created by 木村信行 on 2024/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
